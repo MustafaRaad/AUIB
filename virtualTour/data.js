b86a760dcacf50d176daf2b55c1bc0c5e9f66bb7
MustafaRaad <mustafa.raad.m7@gmail.com>
2021-06-24 08:45:22 +0300

    Add Virtual Tour

---
+++ virtualTour/data.js
@@ -0,0 +1,568 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360-1jpg",
+      "name": "360 (1).jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1574235135380793,
+          "pitch": 0.16774532938184805,
+          "rotation": 0,
+          "target": "1-360-2jpg"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.314705153032449,
+          "pitch": 0.17163850165839278,
+          "title": "To AUIB president office",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.6914542149939376,
+          "pitch": -0.008593261485488668,
+          "title": "Staff House",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.8932133993464664,
+          "pitch": 0.03532726062758407,
+          "title": "Staff House",
+          "text": "To staff houses"
+        }
+      ]
+    },
+    {
+      "id": "1-360-2jpg",
+      "name": "360 (2).jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4302003930469347,
+          "pitch": 0.069357145786757,
+          "rotation": 0,
+          "target": "2-360-3jpg"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4647247113680759,
+          "pitch": -0.09254189964482151,
+          "title": "ELA Building",
+          "text": "English languge academy building"
+        },
+        {
+          "yaw": -1.2562297823071802,
+          "pitch": 0.0990439232366036,
+          "title": "To Sorur Building",
+          "text": "Text"
+        },
+        {
+          "yaw": -3.0095565190411513,
+          "pitch": -0.13005149750814127,
+          "title": "AUIB lack",
+          "text": "The lavk of AUIB"
+        }
+      ]
+    },
+    {
+      "id": "2-360-3jpg",
+      "name": "360 (3).jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.112750987371376,
+          "pitch": 0.0023136211761052294,
+          "rotation": 0,
+          "target": "1-360-2jpg"
+        },
+        {
+          "yaw": -0.10806472472536477,
+          "pitch": 0.06613836672617346,
+          "rotation": 0,
+          "target": "3-360-4jpg"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.9656339439087365,
+          "pitch": -0.013541235916548544,
+          "title": "To ELA building",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.05104044480805037,
+          "pitch": 0.07659318159649331,
+          "title": "To ALFAW buliding",
+          "text": "ALFAW building is th eprimary building in AUIB"
+        }
+      ]
+    },
+    {
+      "id": "3-360-4jpg",
+      "name": "360 (4).jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2692494560427168,
+          "pitch": 0.059785595351087295,
+          "rotation": 0,
+          "target": "4-360-5jpg"
+        },
+        {
+          "yaw": 2.4076930607281684,
+          "pitch": -0.24346143584561375,
+          "rotation": 0,
+          "target": "4-360-5jpg"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.583652530403504,
+          "pitch": -0.23125935842037215,
+          "title": "ALFAW biulding",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.13998402022298784,
+          "pitch": -0.08342204897641992,
+          "title": "AUIB library",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-360-5jpg",
+      "name": "360 (5).jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.15475275507360564,
+          "pitch": 0.031352244917634664,
+          "title": "ALFAW building",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.2596756058975718,
+          "pitch": -0.09439802982442025,
+          "title": "Food court and library",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.4810930752721605,
+          "pitch": -0.10574598194133067,
+          "title": "Main Gate of AUIB",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-360-6jpg",
+      "name": "360 (6).jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-360-7jpg",
+      "name": "360 (7).jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-360-8jpg",
+      "name": "360 (8).jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-360-9jpg",
+      "name": "360 (9).jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-360-10jpg",
+      "name": "360 (10).jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-360-11jpg",
+      "name": "360 (11).jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-360-12jpg",
+      "name": "360 (12).jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-360-13jpg",
+      "name": "360 (13).jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-360-14jpg",
+      "name": "360 (14).jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
